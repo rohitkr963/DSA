09b51c939ce8fae44470e1f5f75d96fcf8d95db0
Rohit Kumar <nk7565331@gmail.com>
2026-09-16 08:09:54 +0530

    solid square

---
+++ PATTERN PRINTING/solidSquare.cpp
@@ -0,0 +1,25 @@
+// ####
+// ####
+// ####
+// ####
+
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int rows, columns;
+    cout<<"Enter number of rows : ";
+    cin>>rows;
+    for(int i = 1; i<=rows; i++)
+    {
+        for(int j = 1; j<=rows; j++)
+        {
+            cout<<"*"<<"";
+        }
+        cout<<endl;
+    }
+}
+
+
