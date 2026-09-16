4b6079178f4eca6c52e994ba638bd46d91d7e511
Rohit Kumar <nk7565331@gmail.com>
2026-09-16 07:45:49 +0530

    power

--- LOOPS 2/power.cpp
+++ LOOPS 2/power.cpp
@@ -8,12 +8,30 @@ int main()
     cin>>a;
     cout<<"Enter number of b : ";
     cin>>b;
-    int power = 1;
+    bool flag = true;   // means positive
+    if(b<0)
+    {
+        flag = false;  // means negative
+        b = -b;
+    }
+    float power = 1;
     for(int i=1; i<=b; i++)
     {
         power *= a;
     }
-    cout<<"The result is: "<<power;
+    if(flag==false)
+    {
+        power = 1/power;
+        b = -b;
+    }
+    if (a== 0 && b == 0)
+    {
+        cout<<"The result is: undefined";
+    }
+    else 
+    {
+        cout<<a<<" raised to the power "<<b<<" is: "<<power;
+    }
 }
 
 
