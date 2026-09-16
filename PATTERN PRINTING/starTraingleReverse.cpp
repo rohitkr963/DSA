1741ff5929f61bbfb605dfc03ea2ac8e28f2f547
Rohit Kumar <nk7565331@gmail.com>
2026-09-16 08:48:45 +0530

    star triagnle reverse

--- PATTERN PRINTING/starTraingleReverse.cpp
+++ PATTERN PRINTING/starTraingleReverse.cpp
@@ -14,7 +14,7 @@ int main()
     cin>>n;
     for(int i = 1; i<=n; i++)
     {
-        for(int j = 1; j<=i; j++)
+        for(int j = i; j<=n; j++)
         {
             cout<<"*"<<"";
         }
