dd0fa76fde409e709cb9588eecd59cb518f5e2b1
Rohit Kumar <nk7565331@gmail.com>
2026-09-18 09:01:00 +0530

    binary triangle

--- PATTERN PRINTING/binaryTriangle.cpp
+++ PATTERN PRINTING/binaryTriangle.cpp
@@ -7,16 +7,24 @@ int main()
     cout<<"Enter number of n : ";
     cin>>n;
     int a = 5;
-    for(int i = 1; i<=n; i++)
-    {
-        if(i % 2 != 0 ) a = 1;   // row no add 
-        else a = 0;              // even
-        for(int j = 1; j<=i; j++)
-        {
-            cout<<a;
-            //fliiping 
-            if(a == 1 ) a = 0;
-            else a= 1;
+    // for(int i = 1; i<=n; i++)
+    // {
+    //     if(i % 2 != 0 ) a = 1;   // row no add 
+    //     else a = 0;              // even
+    //     for(int j = 1; j<=i; j++)
+    //     {
+    //         cout<<a;
+    //         //fliiping 
+    //         if(a == 1 ) a = 0;
+    //         else a= 1;
+    //     }
+    //     cout<<endl;
+    // }
+
+    for(int i = 1; i<=n; i++){
+        for(int j = 1; j<=i; j++){
+            if((i+j) %2 == 0 ) cout<<1;
+            else cout<<0;
         }
         cout<<endl;
     }
